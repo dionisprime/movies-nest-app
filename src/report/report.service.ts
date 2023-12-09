a96d56cf3db236e5f202237e25f159653533e17b
Dionis Prime <dioniskruspe@gmail.com>
2023-12-09 14:35:23 +0300

    delete comments, edit readme

--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -28,24 +28,4 @@ export class ReportService {
 
     return report.save();
   }
-
-  // create(createReportDto: CreateReportDto) {
-  //   return 'This action adds a new report';
-  // }
-
-  // findAll() {
-  //   return `This action returns all report`;
-  // }
-
-  // findOne(id: number) {
-  //   return `This action returns a #${id} report`;
-  // }
-
-  // update(id: number, updateReportDto: UpdateReportDto) {
-  //   return `This action updates a #${id} report`;
-  // }
-
-  // remove(id: number) {
-  //   return `This action removes a #${id} report`;
-  // }
 }
